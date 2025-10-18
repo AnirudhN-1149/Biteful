b79f70864fcc0cafce23a4cbc37da07d8dbdad7a
Anirudh N <anirudhn2005@gmail.com>
2025-10-18 15:47:42 +0530

    Update MongoDB connection URI in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Anirudh:Anirudh2005@cluster0.xmiy1ga.mongodb.net/biteful').then(() => console.log("DB connected"))
+    await mongoose.connect('your_mongodb_uri').then(() => console.log("DB connected"))
 }
