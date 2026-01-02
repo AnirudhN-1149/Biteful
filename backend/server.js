af852649f51c36ccde2954a54476b87ace399e00
AnirudhN-1149 <anirudhn2005@gmail.com>
2026-01-02 10:18:07 +0530

    Improvised server access security

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,15 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
 
 // DB connection
 connectDB();
